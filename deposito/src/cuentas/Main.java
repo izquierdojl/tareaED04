1ba5e74da5a09b232a26a642e4a52b99e300e698
izquierdojl <izquierdo.jl@gmail.com>
2022-03-12 13:10:47 +0100

    Introducido nuevo texto "Saldo generado automáticamente"

--- deposito/src/cuentas/Main.java
+++ deposito/src/cuentas/Main.java
@@ -12,6 +12,7 @@ public class Main {
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("Saldo generado automáticamente" );
 
         try {
             cuenta1.retirar(2300);
